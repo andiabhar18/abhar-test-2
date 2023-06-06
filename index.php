599e53c81e4fcd29d05abda218151db4e811cd81
andiabhar18 <andiabharanakselayar@gmail.com>
2023-06-06 18:24:56 +0700

    menambahkan angka

--- index.php
+++ index.php
@@ -9,5 +9,17 @@
 <body>
   <h1>hello word</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dolores tempore consectetur minus aliquid eligendi cum velit laboriosam ad aliquam odio nostrum, maiores atque ullam accusantium dicta accusamus. Similique, dicta.</p>
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+    <li>6</li>
+    <li>7</li>
+    <li>8</li>
+    <li>9</li>
+    <li>10</li>
+  </ul>
 </body>
 </html>
